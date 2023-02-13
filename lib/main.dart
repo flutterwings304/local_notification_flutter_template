85aaf70b2ef0524b99e50d5fb46a331f9372070c
risheeta jain <risheetajain309@gmail.com>
2023-02-13 22:35:41 +0530

    web version added

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,50 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'api/local_notifications.dart';
+
+Future main() async {
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+Future backgroundHandler(RemoteMessage msg) async {}
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    LocalNotificationService.initialize();
+    LocalNotificationService.messageListener(context);
+    FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+    FirebaseMessaging.instance.getInitialMessage().then((message) {
+      print("Initial Message ${message.toString()}");
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((event) {
+      print("Message on App opened ${event.toString()}");
+    });
+    FirebaseMessaging.onMessage.listen((event) {
+      print("Message when it is termation mode ${event.toString()}");
+      if (event.notification != null) {
+        LocalNotificationService.display(event);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
