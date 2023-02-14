c2dc11b64385b97d80b98ccae041bbc4847baea3
risheeta jain <risheetajain309@gmail.com>
2023-02-14 23:32:14 +0530

    firebase add

--- lib/api/local_notifications.dart
+++ lib/api/local_notifications.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:awesome_notifications/awesome_notifications.dart';
+// import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
@@ -120,25 +120,25 @@ class LocalNotificationService {
         message.notification!.body, platformChannelSpecifics);
   }
 
-  static Future<void> notificationWithImage(RemoteMessage message) async {
-    String? imageUrl;
-    imageUrl ??= message.notification!.android?.imageUrl;
-    imageUrl ??= message.notification!.apple?.imageUrl;
+  // static Future<void> notificationWithImage(RemoteMessage message) async {
+  //   String? imageUrl;
+  //   imageUrl ??= message.notification!.android?.imageUrl;
+  //   imageUrl ??= message.notification!.apple?.imageUrl;
 
-    final id = DateTime.now().millisecondsSinceEpoch ~/ 1000;
-    AwesomeNotifications().createNotification(
-      content: NotificationContent(
-          id: id,
-          icon: "resource://drawable/ic_launcher",
-          channelKey: "Cricstock",
-          title: message.notification!.title,
-          body: message.notification!.body,
-          bigPicture: imageUrl,
-          notificationLayout: NotificationLayout.BigPicture,
-          autoDismissible: true,
-          color: Colors.green),
-    );
-  }
+  //   final id = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+  //   AwesomeNotifications().createNotification(
+  //     content: NotificationContent(
+  //         id: id,
+  //         icon: "resource://drawable/ic_launcher",
+  //         channelKey: "Cricstock",
+  //         title: message.notification!.title,
+  //         body: message.notification!.body,
+  //         bigPicture: imageUrl,
+  //         notificationLayout: NotificationLayout.BigPicture,
+  //         autoDismissible: true,
+  //         color: Colors.green),
+  //   );
+  // }
 }
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'api/local_notifications.dart';
+import 'firebase_options.dart';
 
 Future main() async {
   FirebaseMessaging.onBackgroundMessage(backgroundHandler);
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -23,8 +27,7 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
     LocalNotificationService.initialize();
-    LocalNotificationService.messageListener(context);
-    FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+
     FirebaseMessaging.instance.getInitialMessage().then((message) {
       print("Initial Message ${message.toString()}");
     });
@@ -37,6 +40,7 @@ class _MyAppState extends State<MyApp> {
         LocalNotificationService.display(event);
       }
     });
+    LocalNotificationService.messageListener(context);
   }
 
   @override
